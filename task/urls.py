dbca278836196281646a6adde9216f92e318a882
sorza <alexandre.sorza@outlook.com>
2026-03-24 20:54:45 -0300

    Listando e adicionando dados

--- task/urls.py
+++ task/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index, task_list
+from .views import index, task_list,create_task
 
 urlpatterns = [
     path('', index, name='index'),
     path('tasks/', task_list, name='tasks'),
+    path('create/', create_task, name='create'),
 ]
--- task/views.py
+++ task/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from task.models import Tarefa
 
 def index(request):
     return render(request, 'task/index.html')
 
 def task_list(request):
-    return render(request, 'task/tasks.html')
+    tarefas = Tarefa.objects.all() 
+    return render(request, 'task/tasks.html', {'tarefas': tarefas})
+
+def create_task(request):
+    if request.method == 'POST': 
+        titulo = request.POST.get('titulo')
+        descricao = request.POST.get('descricao')
+        data = request.POST.get('data')
+        status = False
+        Tarefa.objects.create(titulo = titulo, descricao = descricao, data = data, status = status)
+        return redirect('tasks')
+    
+    return render(request, 'task/create_task.html')
